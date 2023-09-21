1f72d47fba1031605bc64e39c256ae1f7c88c0e8
robert <yaoyy13@gmail.com>
2023-09-21 21:12:44 +0800

    testing stuff

--- testing.sh
+++ testing.sh
@@ -5,49 +5,51 @@ command_exists () {
   type "$1" &> /dev/null;
 }
 
-# Define log file
+# Define log directory and file
 LOG_DIR="$HOME/miao-system"
 LOG_FILE="$LOG_DIR/log-file.log"
 
-# Create log directory if it doesn't exist
-mkdir -p $LOG_DIR
+# Ensure we have correct permissions for log directory
+if [ ! -d "$LOG_DIR" ]; then
+    sudo mkdir -p $LOG_DIR
+    sudo chmod 755 $LOG_DIR
+fi
 
-# Create or truncate the log file
-: > $LOG_FILE
+# Initialize or truncate the log file
+sudo : > $LOG_FILE
 
 # Add debugging information to log file
-echo "About to create log directory and file..." | tee -a $LOG_FILE
-echo "Log directory and file should now be created." | tee -a $LOG_FILE
+echo "About to create log directory and file..." | sudo tee -a $LOG_FILE
+echo "Log directory and file should now be created." | sudo tee -a $LOG_FILE
 
-# Check if tcpdump is installed, if not install it
+# Check and install tcpdump if not available
 if ! command_exists tcpdump ; then
-  echo "tcpdump not found. Installing..." | tee -a $LOG_FILE
-  sudo apt-get update
-  sudo apt-get install -y tcpdump
+    echo "tcpdump not found. Installing..." | sudo tee -a $LOG_FILE
+    sudo apt-get update
+    sudo apt-get install -y tcpdump
 fi
 
-# Check if tshark is installed, if not install it
+# Check and install tshark if not available
 if ! command_exists tshark ; then
-  echo "tshark not found. Installing..." | tee -a $LOG_FILE
-  sudo apt-get update
-  sudo apt-get install -y tshark
+    echo "tshark not found. Installing..." | sudo tee -a $LOG_FILE
+    sudo apt-get update
+    sudo apt-get install -y tshark
 fi
 
-# Update and Upgrade
-echo "Starting to update system..." | tee -a $LOG_FILE
-sudo apt-get update
-sudo apt-get upgrade -y
-echo "System update complete." | tee -a $LOG_FILE
+# System update and upgrade
+echo "Starting to update system..." | sudo tee -a $LOG_FILE
+sudo apt-get update && sudo apt-get upgrade -y
+echo "System update complete." | sudo tee -a $LOG_FILE
 
-# Directory to save the capture and backup files
-CAPTURE_DIR="/$HOME/miao-system/capture-pcap"
-CSV_DIR="/$HOME/miao-system/capture-csv"
-BACKUP_DIR="/$HOME/miao-system/backup-pcap"
+# Define directories for pcap, csv, and backup
+CAPTURE_DIR="/path/to/save/pcap_files"
+CSV_DIR="/path/to/save/csv_files"
+BACKUP_DIR="/path/to/save/backup_pcap_files"
 
-# Make sure the directories exist
-mkdir -p $CAPTURE_DIR
-mkdir -p $CSV_DIR
-mkdir -p $BACKUP_DIR
+# Ensure directories exist
+sudo mkdir -p $CAPTURE_DIR
+sudo mkdir -p $CSV_DIR
+sudo mkdir -p $BACKUP_DIR
 
 # List of interfaces to capture on
 INTERFACES=("eth0" "wlan0")
@@ -55,21 +57,50 @@ INTERFACES=("eth0" "wlan0")
 # Packet capture parameters
 PACKET_COUNT=1000
 
+# Flag to monitor success of operations
+ALL_SUCCESS=true
+
 for INTERFACE in "${INTERFACES[@]}"; do
-  # File name
-  PCAP="$CAPTURE_DIR/${INTERFACE}_traffic.pcap"
-  CSV="$CSV_DIR/${INTERFACE}_traffic.csv"
-  BACKUP="$BACKUP_DIR/${INTERFACE}_traffic_backup.pcap"
+    PCAP="$CAPTURE_DIR/${INTERFACE}_traffic.pcap"
+    CSV="$CSV_DIR/${INTERFACE}_traffic.csv"
+    BACKUP="$BACKUP_DIR/${INTERFACE}_traffic_backup.pcap"
 
-  # Capture packets
-  sudo tcpdump -i $INTERFACE -c $PACKET_COUNT -w "$PCAP"
+    # Capture packets
+    sudo tcpdump -i $INTERFACE -c $PACKET_COUNT -w "$PCAP"
 
-  # Backup the PCAP file
-  cp $PCAP $BACKUP
+    # Backup the PCAP file
+    sudo cp $PCAP $BACKUP
 
-  # Convert the PCAP to a simplified CSV format
-  tshark -r "$PCAP" -T fields -e ip.src -e ip.dst -E header=y -E separator=, > "$CSV"
+    # Convert the PCAP to CSV format
+    if ! tshark -r "$PCAP" -T fields \
+        -e ip.src \
+        -e ip.dst \
+        -e tcp.srcport \
+        -e tcp.dstport \
+        -e udp.srcport \
+        -e udp.dstport \
+        -e frame.time_epoch \
+        -e _ws.col.Protocol \
+        -e frame.len \
+        -e dns.qry.name \
+        -e http.request.method \
+        -e http.request.uri \
+        -e http.response.code \
+        -e frame.interface_id \
+        -e frame.direction \
+        -e frame.cap_len \
+        -E header=y \
+        -E separator=, \
+        -E quote=d \
+        -E occurrence=f > "$CSV"; then
+            echo "Failed to convert PCAP to CSV for $INTERFACE." | sudo tee -a $LOG_FILE
+            ALL_SUCCESS=false
+    fi
 done
 
-echo "Data collection and conversion to CSV completed." | tee -a $LOG_FILE
+if [ "$ALL_SUCCESS" = true ]; then
+    echo "Data collection and conversion to CSV completed." | sudo tee -a $LOG_FILE
+else
+    echo "Some operations failed. Check the log for details." | sudo tee -a $LOG_FILE
+fi
 
