54c5772970ed304422fe9f8747a2ae8082171fef
robert <yaoyy13@gmail.com>
2023-10-21 14:55:29 +0800

    make it able to detect the os using as well

--- monitor-connected-devices.sh
+++ monitor-connected-devices.sh
@@ -3,9 +3,12 @@
 # Set the subnet for wlan0
 wlan0_subnet="192.168.20.0/24"
 
+# Temporary file to store host IPs and OS
+temp_file="temp_output.txt"
+
 # Scan for hosts on wlan0
 echo "Scanning for hosts connected to wlan0..."
-nmap -sn $wlan0_subnet -oG - | awk '/Up$/{print $2}' > wlan0_hosts.txt
+sudo nmap -sn -O $wlan0_subnet -oG - | awk '/Up$/{print $2}' > wlan0_hosts.txt
 
 # Count and list the hosts connected to wlan0
 wlan0_count=$(wc -l < wlan0_hosts.txt)
@@ -17,4 +20,15 @@ cat wlan0_hosts.txt
 if [ $wlan0_count -eq 0 ]; then
     echo "No hosts found. Exiting."
     exit 0
+else
+    # Scan again, but this time also attempt to identify the OS
+    sudo nmap -O -iL wlan0_hosts.txt -oG $temp_file
+    awk '/Nmap scan report for/ {printf $5;printf " "}; /Running:/ {print $2}' $temp_file > wlan0_hosts_with_os.txt
+    
+    echo "Hosts with OS:"
+    cat wlan0_hosts_with_os.txt
+
+    # Clean up temporary files
+    rm $temp_file
 fi
+
